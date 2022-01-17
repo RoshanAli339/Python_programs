b3d8369b276d83d9aeace6f6c8a56c558b1f4c78
RoshanAli339 <roshanali339lps@gmail.com>
2022-01-17 20:42:16 +0530

    Monday 17 January 2022 08:41:32 PM IST

--- filter.py
+++ filter.py
@@ -1,5 +1,10 @@
 from PIL import Image, ImageFilter
 
+#you can place your own picture in palce of photo.jpg and it can be of any extension
 before  = Image.open("photo.jpg")
+
+#you can use any filter from the ImageFilter function..(you can select from the list)
 after = before.filter(ImageFilter.EDGE_ENHANCE)
-after.save("out.jpg")
+
+#you can give the output repository your own name and the output file can be of other extension
+after.save("out.bmp")
