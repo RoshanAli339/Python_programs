572f2179ecf307632dad3dba958a8f55dba91ed9
RoshanAli339 <roshanali339lps@gmail.com>
2022-01-18 21:55:26 +0530

    Tuesday 18 January 2022 09:55:26 PM IST

--- push.py
+++ push.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 os.system('git add .')
 
-commit = 'git commit -m "' + datetime.now().strftime("%A %d %B %Y %I:%M:%S %p") +'"'
+commit = 'git commit -m "' + datetime.now().strftime("%A %d %B %Y %I:%M:%S %p") +' IST"'
 os.system(commit)
 
 os.system('git push -u origin main')
